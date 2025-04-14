b0d52f591e0a508fea43f00b60d6f9e48a5bece0
고현민 <hyunmin@gohyeonmins-MacBook-Pro.local>
2025-04-15 00:46:29 +0900

    [Feat] Drag && Drop not complete

---
+++ ALICE/Assets/Script/VisualCardsHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualCardsHandler : MonoBehaviour
+{
+    public static VisualCardsHandler instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
