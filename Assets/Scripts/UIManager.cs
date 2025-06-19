858aa8f47a54cb3adfab20bbff1e25ba98822d0f
qkrgml59 <qkrgml59@gmail.com>
2025-06-19 14:55:50 +0900

    [feat] 게임 설명 추가

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,16 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
 
-    public GameObject HelpPanel;
+    public GameObject helpPanel;
     public TextMeshProUGUI scoreText;
     int score = 0;
+
+    public GameObject[] helpPages;
+    public Button nextButton;
+    public Button prevButton;
+
+    private int currentPage = 0;
+
+
+    private void Start()
+    {
+        ShowPage(currentPage);
+
+        nextButton.onClick.AddListener(NextPage);
+        prevButton.onClick.AddListener(PrevPage);
+    }
+
+
+    void ShowPage(int index)
+    {
+        for (int i = 0; i < helpPages.Length; i++)
+        {
+            helpPages[i].SetActive(i == index);
+        }
+
+        prevButton.interactable = index > 0;
+        nextButton.interactable = index < helpPages.Length - 1;
+
+    }
+
+    void NextPage()
+    {
+        if (currentPage < helpPages.Length -1)
+        {
+            currentPage++;
+            ShowPage(currentPage);
+        }
+    }
+
+    void PrevPage()
+    {
+        if (currentPage > 0)
+        {
+            currentPage--;
+            ShowPage(currentPage);
+        }
+    }
+
+    public void OpenHelpPanel()
+    {
+        helpPanel.SetActive(true);
+        currentPage = 0;
+        ShowPage(currentPage);
+        
+    }
+
+    public void CloseHelpPanel()
+    {
+        helpPanel.SetActive(false);
+    }
+
 
     void Awake()
     {
@@ -43,15 +105,7 @@ public class UIManager : MonoBehaviour
     }
 
 
-    public void OpenOptionsPanel()
-    {
-        HelpPanel.SetActive(true);
-    }
-
-    public void CloseHelpPanel()
-    {
-        HelpPanel.SetActive(false);
-    }
+  
 
     public void OnClickStar()
     {
