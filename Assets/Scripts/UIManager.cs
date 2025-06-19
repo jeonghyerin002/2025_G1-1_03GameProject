2b5d2bfb83a0ac87e8e4f8a5861700fddbfc51c1
jeonghyerin002 <jhr102446@gmail.com>
2025-06-19 16:10:44 +0900

    그거그거그거

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Search;
+//using UnityEditor.Search;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
